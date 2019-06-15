8038ae446977957d169568a60e6a6af7ace5aa56
Trevor Lyons <trevorjclyons@gmail.com>
2019-06-15 17:53:43 +0200

    first project commit

---
+++ waracleProject/waracleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  waracleProject
+//
+//  Created by Trevor Lyons on 15/06/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
